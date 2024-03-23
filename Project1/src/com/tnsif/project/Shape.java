30ae7d64c3e99b5242fd3bdf97f300e66cf460fb
Ramling-A-Biradar <ramlingbiradar999@gmail.com>
2024-03-23 22:12:10 +0530

    Second Commit

---
+++ Project1/src/com/tnsif/project/Shape.java
@@ -0,0 +1,13 @@
+package com.tnsif.project;
+
+public class Shape {
+	int length=10;
+	int breadth=20;
+	int ans;
+	void area()
+	{
+		ans=length*breadth;
+		System.out.println("Area is :"+ans);
+		}
+
+}
---
+++ Project1/src/com/tnsif/project/ShapeDemo.java
@@ -0,0 +1,16 @@
+package com.tnsif.project;
+
+public class ShapeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shape s=new Shape();
+		
+		System.out.println("Length :"+s.length);
+		System.out.println("Breadth is :"+s.breadth);
+		s.area();
+		
+
+	}
+
+}
